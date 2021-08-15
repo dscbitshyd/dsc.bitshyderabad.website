b1830b3cf61ff5b4861ba4aee126f5830b0569ff
Sai Ankit <saiankit30@gmail.com>
2021-08-15 18:31:47 +0530

    update discord link

--- components/header/DiscordJoinCommunity.jsx
+++ components/header/DiscordJoinCommunity.jsx
@@ -4,7 +4,7 @@ function DiscordJoinCommunity() {
   const style = { color: 'white', fontSize: '1em' };
 
   return (
-    <a href="https://discord.gg/F2QF5eG9">
+    <a href="https://discord.gg/qMZDZ2NpMB">
       <div className="flex gap-2 items-center bg-google-blue hover:bg-google-blue font-bold p-2 rounded transition duration-500 hover:scale-110">
         <FaDiscord style={style} />
         <h1 style={style} className="text-red">
